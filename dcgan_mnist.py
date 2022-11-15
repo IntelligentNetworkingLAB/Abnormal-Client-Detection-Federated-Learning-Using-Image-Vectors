9754ad15d77c7d54b80813a8151c9831adee8a58
IntelligentNetworkingLAB <48087803+IntelligentNetworkingLAB@users.noreply.github.com>
2022-11-15 19:41:15 +0900

    Add files via upload

--- dcgan_mnist.py
+++ dcgan_mnist.py
@@ -166,7 +166,19 @@ def train(dataset, epochs):
                            epochs,
                            seed)
 
+def generate_and_save_images(model, epoch, test_input):
+  
+  predictions = model(test_input, training=False)
 
+  fig = plt.figure(figsize=(10,10))
+
+  for i in range(predictions.shape[0]):
+      plt.subplot(10, 10, i+1)
+      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+      plt.axis('off')
+
+  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.show()
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%time
